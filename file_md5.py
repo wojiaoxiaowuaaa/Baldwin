54c0b0ac825ce8ebac40c99bc6e6412655f5cea6
xiaowu <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2023-12-28 19:20:40 +0800

    delete ks

--- file_md5.py
+++ file_md5.py
@@ -7,7 +7,7 @@ def calculate_md5(file_path, block_size=1024):
     with open(file_path, 'rb') as file:
         for block in iter(lambda: file.read(block_size), b''):
             md5.update(block)  # 这个方法会更新 MD5 对象的状态，考虑到了新的数据块。在整个文件读取过程中，这个方法会不断更新 MD5 对象，将所有的数据块一一加入计算。
-    return md5.hexdigest()
+    return md5.hexdigest()  # 获取 MD5 摘要的十六进制表示
 
 
 md5_value = calculate_md5(os.path.abspath(__file__))
