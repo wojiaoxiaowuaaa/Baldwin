ddae51cb527aee965e85f80873f95a659a176e2b
xiaowu <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-01-11 15:59:26 +0800

    20240111 15:59

--- youtube_down.py
+++ youtube_down.py
@@ -1,23 +1,31 @@
 from pytube import YouTube
-from time_count import timer
+from time_count import calculate_execution_time
 
 
-@timer
-def download_youtube_video(url, save_path):
+@calculate_execution_time
+def download_youtube_video(url, output_path='.'):
     try:
+        # 创建 YouTube 对象
         yt = YouTube(url)
-        stream = yt.streams.get_highest_resolution()  # 获取最高分辨率的视频流
 
-        stream.download(output_path=save_path)  # 下载视频到指定路径
+        # 获取视频的最高分辨率
+        video = yt.streams.get_highest_resolution()
 
-        print(f"视频已成功下载到 {save_path}")
+        # 下载视频到指定路径
+        video.download(output_path)
 
+        print(f"视频 '{yt.title}' 下载完成！")
     except Exception as e:
-        print(f"下载视频时出错：{e}")
+        print(f"下载失败：{e}")
 
 
 if __name__ == "__main__":
-    youtube_url = """
-        https://www.douyin.com/video/7285611499786079507
+    # 替换为你要下载的 YouTube 视频的 URL
+    video_url = """
+        https://www.youtube.com/watch?v=055mU4f3E5A
     """
-    download_youtube_video(youtube_url, '.')
+
+    # 替换为你想要保存视频的本地路径
+    # save_path = "path/to/save"
+
+    download_youtube_video(video_url)
