5728a4072b4ddead5c836bb7bfddd28b2fcf693d
xiaowu <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-01-19 10:57:03 +0800

    0119

---
+++ download_kit_ticktock.py
@@ -0,0 +1,15 @@
+from DownloadKit import DownloadKit
+
+
+def download_kit_ticktock():
+    # 创建下载器对象
+    d = DownloadKit('/Users/wl/Desktop')
+
+    # 遍历文件获取URL地址 传参到下载器对象进行下载
+    with open('doc/url.txt', 'r') as f:
+        for i in f:
+            d.download(i)
+
+
+if __name__ == '__main__':
+    download_kit_ticktock()
