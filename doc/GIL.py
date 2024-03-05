948cbe37aeb630a5bc8f27409a303c56bc05506c
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-03-05 20:01:55 +0800

    0305

--- doc/GIL.py
+++ doc/GIL.py
@@ -1,8 +1,12 @@
-# 全局解释器锁（Global Interpreter Lock，简称GIL）是Python解释器中的一个重要概念。它是一种互斥锁，用于确保在任何给定时刻只有一个线程在解释器中执行Python字节码，即在单个进程内，Python
+# 全局解释器锁（Global Interpreter Lock，简称GIL）是Python解释器中的一个重要概念。它是一种互斥锁，
+# 用于确保在任何给定时刻只有一个线程在解释器中执行Python字节码，即在单个进程内，Python
 # 解释器的并发性被限制在一个线程上执行。这意味着在多线程程序中，多个线程不能同时利用多核处理器的能力。
-# GIL的存在主要是为了简化Python解释器的实现，以避免在解释器内部处理线程间的资源竞争和复杂性。然而，它也导致了Python在CPU密集型任务上的性能限制。对于I/O密集型任务，GIL通常不会成为性能瓶颈，因为线程在等待I/O操作时会释放GIL。 下面是一个简单的示例来说明GIL的影响。考虑一个计算密集型任务，计算一个大素数列表的示例：
+# GIL的存在主要是为了简化Python解释器的实现，以避免在解释器内部处理线程间的资源竞争和复杂性。
+# 然而，它也导致了Python在CPU密集型任务上的性能限制。对于I/O密集型任务，GIL通常不会成为性能瓶颈，
+# 因为线程在等待I/O操作时会释放GIL.下面是一个简单的示例来说明GIL的影响.考虑一个计算密集型任务计算一个大素数列表的示例：
 
 import threading
+import time
 
 
 # 计算素数的函数
@@ -10,20 +14,25 @@ def calculate_prime(n):
     primes = []
     for num in range(2, n + 1):
         # all() 是Python内置函数之一，用于判断可迭代对象中的所有元素是否都为真（True）。如果可迭代对象中的所有元素都为真，all() 函数返回 True；否则，返回 False
-        if all(num % i != 0 for i in range(2, int(num**0.5) + 1)):
+        if all(num % i != 0 for i in range(2, int(num ** 0.5) + 1)):
             primes.append(num)
     return primes
 
 
-# 创建两个线程来同时计算素数
-t1 = threading.Thread(target=calculate_prime, args=(50000,))
-t2 = threading.Thread(target=calculate_prime, args=(60000,))
+start = time.time()
+
+# 创建两个线程来计算素数
+t1 = threading.Thread(target=calculate_prime, args=(500000,))
+t2 = threading.Thread(target=calculate_prime, args=(500000,))
 
 t1.start()
 t2.start()
 
 t1.join()
 t2.join()
+
+# 使用多进程这里可以节约一倍的时间
+print(time.time() - start)
 
 # 尽管我们使用了两个线程来计算不同的素数列表，但由于GIL的存在，这两个线程不会并行执行，而是交替执行。这导致多线程在计算密集型任务上无法充分利用多核处理器的性能。
 # 要注意，GIL只影响了CPython（Python的标准实现）。其他Python实现，如Jython（用于Java虚拟机）或IronPython（用于.NET Framework），不一定有GIL，因此在某些情况下可以更好地利用多核处理器。 对于解决GIL的问题，可以考虑以下方法：
@@ -44,3 +53,4 @@ Python解释器使用全局解释器锁（Global Interpreter Lock，GIL）来确
 这个主进程通常是由您的操作系统分配的，它会在执行脚本时启动，执行脚本中的代码，然后在脚本执行完成后退出。如果您在脚本中使用多线程或多进程，还会创建相应数量的子线程或子进程，这些子线程或子进程由主进程管理。
 主进程的行为会受到操作系统的管理，包括进程的创建、销毁、资源管理等。如果您在脚本中使用多线程或多进程，主进程也会负责管理这些辅助线程或子进程。总之，主进程是执行Python脚本的起点，它是您的脚本在操作系统中的代表。
 """
+
---
+++ more_queue.py
@@ -0,0 +1,42 @@
+import multiprocessing
+import time
+import os
+
+
+def producer(queue):
+    # 负责向共享队列中放入数据。
+    print(os.getpid(), "Producer starts")
+    for i in range(5):
+        time.sleep(3)
+        data = f"Message {i}"
+        print(f"Producer puts: {data}")
+        queue.put(data)
+
+
+def consumer(queue):
+    # 负责从共享队列中取出数据。
+    print(os.getpid(), "Consumer starts")
+    for i in range(5):
+        time.sleep(3)
+        data = queue.get()
+        print(f"Consumer gets: {data}")
+        print("\033[1;32m" + "*" * 30 + "\033[0m")
+
+
+if __name__ == "__main__":
+    # 主进程是整个脚本的执行起点,主要负责创建并控制其他两个子进程。
+    print(os.getpid(), "Main process starts")
+    # 创建共享队列
+    shared_queue = multiprocessing.Manager().Queue()
+
+    # 创建两个进程，一个作为生产者，一个作为消费者
+    producer_process = multiprocessing.Process(target=producer, args=(shared_queue,))
+    consumer_process = multiprocessing.Process(target=consumer, args=(shared_queue,))
+
+    # 启动进程
+    producer_process.start()
+    consumer_process.start()
+
+    # 主进程通过 join() 等待两个子进程的结束，以保证整个脚本的正确执行.
+    producer_process.join()
+    consumer_process.join()
