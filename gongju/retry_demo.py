7f9d76eb5450484fb0ff92aef724f2c486dce93d
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-03-24 16:57:28 +0800

    0324

--- gongju/retry_demo.py
+++ gongju/retry_demo.py
@@ -1,10 +1,19 @@
 from tenacity import retry, stop_after_attempt, wait_fixed
+from retry_decorator import retry_
 
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
 def my_function():
     print('ing......')  # 重试三次  时间间隔为1秒
-    assert 1 == 2
+    # assert 1 == 2
+    # raise Exception('error')
 
 
-my_function()
+@retry_(5, 5)
+def test_func():
+    print('test_func is called')
+    assert 1 == 1
+
+
+# my_function()
+test_func()
