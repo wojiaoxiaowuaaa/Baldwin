a4dee3ef8739e68a2640c1c7d28fc62f6d15b737
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-04-07 20:10:33 +0800

    0407 json

--- json_dic.py
+++ json_dic.py
@@ -1,15 +1,16 @@
 import json
+import os
 
 
 def func(json_file):
-    """读取本地存储的json文件  转化为dict类型后在脚本中作数据分析用.
+    """读取本地存储的json文件,转化为dict类型后在脚本中作数据分析用.
     json.load 用于从文件中加载 JSON 数据，接收文件对象作为参数,例如f。
-    json.loads 用于从字符串中加载 JSON 数据，接收字符串作为参数例如f.read()."""
+    json.loads 用于从字符串中加载 JSON 数据，接收字符串作为参数例如f.read()或变量名"""
     with open(json_file, 'r') as f:
         data = json.load(f)  # <class 'dict'>
         return data
 
 
-json_file = '/Users/wl/Desktop/laocations_nearby.json'
+json_file = f'{os.path.dirname(__file__)}/doc/demo.json'
 data = func(json_file)
 print(data)
