2154a7b1d26bf78e707323a6637171d7ed8979eb
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-04-26 18:11:02 +0800

    0426

--- suanfa/index_sum.py
+++ suanfa/index_sum.py
@@ -1,16 +1,22 @@
-def func(l, target):
+"""根据两数之和 找出两数的下标"""
+
+
+def func(ll, tar):
     hashmap = {}
 
-    for index, value in enumerate(l):
+    # enumerate() 是 Python 内置函数之一，它用于将一个可迭代对象（如列表、元组、字符串等）组合为一个枚举对象，可以同时获取索引和对应的元素值。
+    for index, value in enumerate(ll):
         hashmap[value] = index
 
-    for i, v in enumerate(l):
-        lord = hashmap.get(target - v)
+    for k, v in enumerate(ll):
+        # 对于列表中的每个元素 v，查找是否存在另一个数 tar - v
+        j = hashmap.get(tar - v)
 
-        if lord is not None and lord != i:
-            return i, lord
+        # 在字典 hashmap 中。如果存在&&下标不与当前元素的下标相同，说明找到了符合条件的两个数，即它们的和等于目标值 target。
+        if j is not None and j != k:
+            return k, j
 
 
 l = [1, 3, 7, 11]
-target = 10
+target = 12
 print(func(l, target))
