a2b754ff29b1fea0e3935a26c39da724cd68df7e
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-04-30 20:38:45 +0800

    0430

--- operate_dic.py
+++ operate_dic.py
@@ -1,45 +1,12 @@
 import os
-from color_pr import color_print_green
-from loguru import logger
-
-
-# 遍历文件夹查找指定格式的文件
-def find_func(file, pwd):
-    kings = []
-    # os.walk：Python 的标准库函数，用于以递归的方式遍历目录树。
-    # 它返回一个生成器，每次迭代都会返回一个包含三个元素的元组：当前目录的路径，当前目录下的子目录列表，以及当前目录下的文件列表。
-    for root, dirs, files in os.walk(pwd):
-        for f in files:
-            name, postfix = os.path.splitext(f)
-            if postfix == file:
-                kings.append(f)
-    return len(kings), kings
-
-
-def total_func(pwd):
-    # walk()是 Python 中用于遍历目录树的函数。它接受一个起始目录路径作为参数，并返回一个生成器，用于生成目录树中每个目录的信息.
-    for root, dirs, files in os.walk(pwd):
-        logger.info(f"Directory: {root}")
-        # print(f"Total files: {len(files)}")
-        total_size = sum(os.path.getsize(os.path.join(root, name)) for name in files)
-        # print(f"Total size: {total_size} bytes")
-        # color_print_green()
-
-
-# landing = find_func('.py', '/Users/wl/Desktop')
-# print(landing[0])
-
-total_func('/Users/wl/go')
-
-"""
 import asyncio
-import os
 import multiprocessing
 from loguru import logger
 
 
 async def get_directory_info(directory):
     # 多进程+多任务
+    # async for 语法用于异步迭代，但是在这个脚本中，os.walk() 并不是异步操作，因此无法直接在 async for 中使用。
     for root, dirs, files in os.walk(directory):
         logger.info(f"Directory: {root}")
         # for name in files:
@@ -55,12 +22,27 @@ def process_func(directory):
 
 if __name__ == "__main__":
     processes = []
-    for _ in range(multiprocessing.cpu_count()):  # 使用CPU核心数作为进程数量
+    for _ in range(multiprocessing.cpu_count()):
         p = multiprocessing.Process(target=process_func, args=("/Users/wl/go",))
         processes.append(p)
         p.start()
 
     for p in processes:
         p.join()
+
+"""
+
+def total_func(pwd):
+    # walk()是 Python 中用于遍历目录树的函数。它接受一个起始目录路径作为参数，并返回一个生成器，用于生成目录树中每个目录的信息.
+    for root, dirs, files in os.walk(pwd):
+        logger.info(f"Directory: {root}")
+        # print(f"Total files: {len(files)}")
+        total_size = sum(os.path.getsize(os.path.join(root, name)) for name in files)
+        # print(f"Total size: {total_size} bytes")
+        # color_print_green()
+
+
+landing = find_func('.py', '/Users/wl/Desktop')
+print(landing[0])
 
 """
---
+++ suanfa/find_file.py
@@ -0,0 +1,18 @@
+from color_pr import color_print_green
+import os
+
+
+def find_func(file, pwd):
+    # 遍历文件夹查找指定格式的文件
+    kings = []
+    # os.walk：Python 的标准库函数，用于以递归的方式遍历目录树。
+    # 它返回一个生成器，每次迭代都会返回一个包含三个元素的元组：当前目录的路径，当前目录下的子目录列表，以及当前目录下的文件列表。
+    for root, dirs, files in os.walk(pwd):
+        for f in files:
+            name, postfix = os.path.splitext(f)
+            if postfix == file:
+                kings.append(f)
+    return len(kings), kings
+
+
+print(find_func('.py', '/Users/wl/Desktop'))
