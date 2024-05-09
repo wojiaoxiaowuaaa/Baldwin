b3b5196ef940cb4e2b0acd31ea120b8e3052b9b2
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-05-09 19:16:19 +0800

    0509

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 """
 ---介绍下sys.path.append(os.path.abspath('.'))
 
@@ -8,11 +11,13 @@ sys.path.append(path) 是将指定的路径 path 添加到 sys.path 列表中。
 
 sys.path 是一个列表，它指定了 Python 在导入模块时会搜索的目录。
 当你尝试导入一个模块时，Python 会按照 sys.path 中的目录顺序依次查找模块文件。
+
+将当前目录添加到系统路径中。这样，当你在代码中使用import语句导入模块时，Python 将会在当前目录及其子目录中查找模块文件。
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+for i in sys.path: print(i)
 """
 
 
-import os
-import sys
-# 将当前目录添加到系统路径中。这样，当你在代码中使用import语句导入模块时，Python 将会在当前目录及其子目录中查找模块文件。
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# for i in sys.path: print(i)
+# 项目根路径
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_PATH)
