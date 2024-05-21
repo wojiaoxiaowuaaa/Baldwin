155dead931b2aa98b869acf35c1c395f6d565919
wb_wanglei12 <wb_wanglei12@kuaishou.com>
2024-05-21 20:00:21 +0800

    0521

--- doc/单例模式.py
+++ doc/单例模式.py
@@ -21,3 +21,20 @@ class Singleton:
 a = Singleton()
 b = Singleton()
 print(a is b)
+
+
+"""
+
+class User:
+    __instance = None  # 类变量  初始化为None  存储唯一的实例对象
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:  # 重写new方法  创建实例之前检查是否已经存在实例
+            cls.__instance = super().__new__(cls, *args, **kwargs)  # 调用父类的 __new__ 方法，创建一个新的实例，并将其赋值给 __instance。这里调用父类的 __new__ 方法，确保正确地创建实例。
+        return cls.__instance  # 返回存储在 __instance 中的唯一实例。
+        
+
+一般情况下，__new__ 方法不需要手动调用，因为在对象创建时，Python 解释器会自动调用它。
+总结一下，__new__ 负责对象的创建和内存的分配，而 __init__ 方法则负责对象的初始化。在很多情况下，你只需要重写 __init__ 方法，因为大部分对象的创建和初始化都可以在这个方法中完成。
+
+"""
