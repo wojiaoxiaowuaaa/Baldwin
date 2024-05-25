ca2038abda1858e3a56d0a9c50ec7f8b68a5b1c2
mac <18506832850@163.com>
2024-05-25 13:30:26 +0800

    home 2

--- gongju/upload_file.py
+++ gongju/upload_file.py
@@ -9,4 +9,25 @@ def upload_file(url, pwd):
 
 
 # 上传文件时，服务器必须支持文件上传,并正确处理 multipart/form-data 类型的请求.
-print(upload_file('http://httpbin.org/post', os.path.abspath(__file__)))
+# print(upload_file('http://httpbin.org/post', os.path.abspath(__file__)))
+print(upload_file('http://127.0.0.1:5000/upload', os.path.abspath(__file__)))
+
+
+# from flask import Flask, request
+#
+# app = Flask(__name__)
+#
+# @app.route('/upload', methods=['POST'])
+# def upload():
+#     file = request.files['file']
+#     from loguru import logger
+#     logger.info(file)
+#     logger.info(dir(file))
+#     if file:
+#         file.save('./uploads/' + file.filename)
+#         return '文件上传成功'
+#     else:
+#         return '未找到文件'
+#
+# if __name__ == '__main__':
+#     app.run()
