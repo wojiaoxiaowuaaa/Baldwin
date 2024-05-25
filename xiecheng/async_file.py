bd88a1e40a7aebfa93162e3172a74a0996553b00
mac <18506832850@163.com>
2024-05-25 23:38:17 +0800

    0525

--- xiecheng/async_file.py
+++ xiecheng/async_file.py
@@ -18,10 +18,15 @@ async def read_all_files_in_directory():
 
 
 async def main():
+    # 这里的result是列表  列表中元素为read_file任务中返回的元祖
     result = await read_all_files_in_directory()
     # 如果要在协程中迭代一个可迭代对象（如列表、元组等），可以直接使用for循环。协程会自动处理迭代过程中的异步操作。
-    for i in result:
-        logger.info(f"文件名:{i[0]}  文件内容:\n{i[1]}")
+    # for i in result:
+    #     logger.info(f"文件名:{i[0]}  文件内容:\n{i[1]}")
+    # for k, v in dict(result).items():
+    #     logger.info(f"文件名:{k}  文件内容:\n{v}")
+    for i in dict(result):
+        logger.info(f"文件名:{i}  文件内容:\n{dict(result)[i]}")
 
 
 if __name__ == "__main__":
