1908d4a49c565e77ea424c86c0a6081963c10647
wl <1734455@qq.com>
2024-07-18 12:48:24 +0800

    log

--- gongju/log_register.py
+++ gongju/log_register.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 -------------------------------------------------
-   File Name:     LogHandler.py
+   File Name:     log_register.py
    Description:   日志操作模块
    Author:        JHao
    date:          2017/3/6
@@ -12,8 +12,6 @@
                    2020/07/13: Windows下TimedRotatingFileHandler线程不安全, 不再使用
 -------------------------------------------------
 """
-__author__ = "JHao"
-
 import os
 import logging
 import platform
@@ -34,9 +32,9 @@ CURRENT_PATH: str = os.path.dirname(os.path.abspath(__file__))
 ROOT_PATH: str = os.path.join(CURRENT_PATH, os.pardir)
 LOG_PATH: str = os.path.join(ROOT_PATH, "config/log")
 
-# 当你在其他脚本中导入并实例化LogHandler对象时,if 代码块会触发,但这种触发发生在首次导入模块时,而不是在每次创建LogHandler实例时.这是因为Python模块的导入机制.当一个模块首次被导入时.模块内的所有顶层代码(即不在任何函数或类定义内部的代码)都会被执行一次.
-# 当你在另一个脚本中通过import log_handler或from log_handler import LogHandler导入LogHandler时if语句将会执行,创建LOG_PATH目录（如果它还不存在的话）之后,无论你在这个脚本中创建多少个LogHandler实例,if语句中的代码都不会再次执行,因为模块已经加载完毕,其顶层代码只会被执行一次.
-# 因此，将日志目录创建的逻辑放在模块的顶层，可以确保在任何LogHandler实例被创建之前，日志目录已经被创建好了，这有助于避免在日志记录过程中遇到目录不存在的错误。同时，这也避免了每次实例化LogHandler时都重复执行目录创建逻辑，提高了效率。
+# 当你在其他脚本中导入并实例化LogRegister对象时,if代码块会触发,但这种触发发生在首次导入模块时,而不是在每次创建LogRegister实例时.这是因为Python模块的导入机制.当一个模块首次被导入时.模块内的所有顶层代码(即不在任何函数或类定义内部的代码)都会被执行一次.
+# 当你在另一个脚本中通过import log_register或from log_register import LogRegister导入LogRegister时if语句将会执行,创建LOG_PATH目录（如果它还不存在的话）之后,无论你在这个脚本中创建多少个LogRegister实例,if语句中的代码都不会再次执行,因为模块已经加载完毕,其顶层代码只会被执行一次.
+# 因此日志目录创建的逻辑放在模块的顶层,可以确保在任何LogRegister实例被创建之前,日志目录已经被创建好了,这有助于避免在日志记录过程中遇到目录不存在的错误。同时,这也避免了每次实例化LogRegister时都重复执行目录创建逻辑提高了效率.
 if not os.path.exists(LOG_PATH):
     try:
         os.mkdir(LOG_PATH)
@@ -105,5 +103,5 @@ class LogRegister(logging.Logger):
 
 
 # if __name__ == '__main__':
-#     log = LogHandler('test')
+#     log = LogHandler('test', level=WARNING)
 #     log.info('this is a test msg')
