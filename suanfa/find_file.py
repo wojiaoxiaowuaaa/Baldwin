4f45770332c4f4138520627b377163678febe915
wl <1734455@qq.com>
2024-07-19 16:22:19 +0800

    refactor: remove unnecessary sys.path modification and banner comments
    - Remove commented out code for sys.path manipulation in __init__.py.
    - Eliminate unused banner comments in color_pr.py and gongju/xing_neng.py.
    -

--- suanfa/find_file.py
+++ suanfa/find_file.py
@@ -2,26 +2,28 @@ import os
 from pathlib import Path
 
 
-def find_func(file, pwd):
-    # 遍历文件夹查找指定格式的文件并进行数据统计
-    # find_func('.py', '/Users/wl/Desktop')
-    kings = []
-    # os.walk：Python 的标准库函数，用于以递归的方式遍历目录树。
-    # 它返回一个生成器，每次迭代都会返回一个包含三个元素的元组：当前目录的路径，当前目录下的子目录列表，以及当前目录下的文件列表。
+def find_file(file: str, pwd: str) -> tuple:
+    """
+    遍历文件夹查找指定格式(后缀)的文件并进行数据统计
+    os.walk：Python 的标准库函数用于以递归的方式遍历目录树 它返回一个生成器 每次迭代都会返回一个包含三个元素的元组：当前目录的路径，当前目录下的子目录列表，以及当前目录下的文件列表.
+    :param file: 文件后缀
+    :param pwd: 文件夹路径
+    """
+    results = []
     for root, dirs, files in os.walk(pwd):
         for f in files:
             name, postfix = os.path.splitext(f)
             if postfix == file:
-                kings.append(f)
-    return len(kings), kings
+                results.append(f)
+
+    return len(results), results
 
 
-def find_files(directory: Path, pattern: str):
+def find_file01(directory: Path, pattern: str) -> list[Path]:
     """
     遍历指定目录及其子目录，查找匹配指定模式的文件。
-
-    :param directory: 要遍历的目录路径
-    :param pattern: 文件匹配dou模式（例如 '*.txt'）
+    :param directory: 要遍历的目录路径的Path对象 (例如 Path("/Users/wl/Downloads"))
+    :param pattern: 文件匹配模式（例如 '*.txt'）
     :return: 匹配的文件列表
     """
     if not directory.is_dir():
@@ -32,6 +34,16 @@ def find_files(directory: Path, pattern: str):
     return matched_files
 
 
-# var = find_files(Path.cwd(), '*.txt')
-for i in (var := find_files(Path.cwd(), '*.py')): print(i)
+def find_file02(file: str, pwd: str) -> None:
+    """
+    遍历目录查找指定的文件文件名所在的绝对路径 这里的文件名必须是完整的不能使用通配符
+    :param file: 文件名
+    :param pwd: 目录路径
+    """
+    for root, dirs, files in os.walk(pwd):
+        if file in files:
+            result = os.path.join(root, file)
+            print(f"目标文件的路径是:{result}")
 
+
+# for i in (var := find_file01(Path.cwd(), "*.py")): print(i)
