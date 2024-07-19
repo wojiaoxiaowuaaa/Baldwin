46723c904f5b82e6febd14ca2423a0ac476a3ee0
sdfsd <sdsdgg@qq.com>
2024-07-19 19:20:04 +0800

    qa

---
+++ gongju/qr_png.py
@@ -0,0 +1,21 @@
+import qrcode
+from matplotlib import pyplot as plt
+
+
+def qr_png(data):
+    """生成二维码
+    :param data: 二维码内容"""
+    qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill='black', back_color='white')
+    # img.save("qrcode.png")
+
+    fig = plt.figure()  # 使用matplotlib展示二维码
+    ax = fig.add_subplot(1, 1, 1)
+    ax.imshow(img)
+    ax.axis('off')  # 不显示坐标轴 使得展示的图片更加简洁
+    plt.show()
+
+
+qr_png('test')
--- gunicorn_flask.py
+++ gunicorn_flask.py
@@ -10,21 +10,20 @@ def hello_world(): return 'Hello, World!'
 
 
 class FlaskApplication(BaseApplication):
-    """使用Gunicorn作为WSGI服务器来部署一个Flask应用.构造函数接收一个Flask应用实例和一个可选的配置字典.super()调用父类的构造函数,初始化基类."""
+    """使用Gunicorn作为WSGI服务器来部署一个Flask应用.构造函数接收一个Flask应用实例和一个可选的配置字典. super()调用父类的构造函数,初始化基类."""
     def __init__(self, app, options=None):
         self.application = app
         self.options = options or {}
-        super(FlaskApplication, self).__init__()
+        super().__init__()
 
     def load_config(self):
-        """子类重写父类的抽象方法 load_config方法负责从options字典中加载Gunicorn的配置.它遍历字典,只选取那些在self.cfg.settings中存在的键.并且值不为None的项,然后将它们设置到Gunicorn的配置中."""
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
+        """子类重写父类的抽象方法 load_config方法负责从options字典中加载Gunicorn的配置.它遍历字典,只选取那些在self.cfg.settings中存在的键,并且值不为None的项,然后将它们设置到Gunicorn的配置中."""
+        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
         for key, value in config.items():
             self.cfg.set(key.lower(), value)
 
     def load(self):
-        """子类重写父类的抽象方法 load方法返回Flask应用实例 Gunicorn将使用这个实例来处理HTTP请求"""
+        """子类重写父类的抽象方法 load方法返回Flask应用实例 Gunicorn 将使用这个实例来处理HTTP请求"""
         return self.application
 
 
