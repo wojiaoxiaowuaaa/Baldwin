3e2ab8c8d95929ef63cda6077e4d6003e0476f9c
bugger <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-08-13 18:04:09 +0800

    hebing

--- gongju/dir_file.py
+++ gongju/dir_file.py
@@ -295,7 +295,6 @@ def calculate_file_size(file_path):
 import asyncio
 import os
 
-
 # async def async_walk(pwd):
 #     # 用于异步遍历目录的函数 返回指定目录下的所有文件的绝对路径
 #     for root, dirs, filenames in os.walk(pwd):
@@ -306,7 +305,7 @@ import os
 #             # yield 语句的作用是将每个文件的路径作为生成器的值返回给调用者.它实现了一种惰性计算的方式,在每次迭代时生成一个文件路径,而不是一次性将所有文件路径都生成出来.这种方式可以节省内存,特别是在处理大量文件时.
 #             yield os.path.join(root, filename)
 #
-# 
+#
 # async def main():
 #     ll = []
 #     # async for 循环会等待异步生成器产生的值,而异步生成器会在需要时异步地生成值并将其返回给循环.
