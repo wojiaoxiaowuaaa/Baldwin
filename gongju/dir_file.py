f2854712e803d2f446284ec9166706c666311bea
bugger <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-08-13 16:58:56 +0800

    hebing

--- gongju/dir_file.py
+++ gongju/dir_file.py
@@ -44,6 +44,8 @@ import sys
 import subprocess
 import shutil
 from pathlib import Path
+import requests
+import uuid
 
 
 def clean_dir(dir):
@@ -235,6 +237,29 @@ def find_file02(file: str, pwd: str) -> None:
             print(result)
 
 
+def download_images_from_file(filename, output_dir):
+    """根据指定文件中的URL下载图片到指定目录"""
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    with open(filename, "r") as file:
+        for line in file:
+            url = (
+                line.strip()
+            )  # strip()方法移除字符串开头和结尾的指定字符(默认是空白字符、空格、换行符、制表符等)
+            response = requests.get(url)
+            if response.status_code == 200:
+                image_name = str(uuid.uuid4()).replace("-", "") + ".webp"
+                save_path = os.path.join(output_dir, image_name)
+                with open(save_path, "wb") as f:
+                    f.write(response.content)  # response.content 是 requests 库中的一个属性,它包含了服务器返回的原始响应数据.对于图片资源,response.content 包含的是图片的二进制数据.
+                print(f"已下载图片:{save_path}")
+            else:
+                print(f"无法下载图片:{url}")
+
+    # shutil.rmtree(output_dir)
+
+
 if __name__ == "__main__":
     ...
     # clean_dir(".idea")
@@ -257,3 +282,5 @@ if __name__ == "__main__":
 
     # for i in (var := find_file01(Path.cwd().parent, "*.py")): print(i)
     # find_file02("__init__.py", "/Users/wl/Downloads")
+
+    # download_images_from_file("../config/url.txt", "../config/pic")
