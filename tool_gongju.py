2584afe1329a741b6b01c8df220f120031ecc3ce
bugger <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-08-13 17:40:02 +0800

    hebing

--- tool_gongju.py
+++ tool_gongju.py
@@ -1,27 +1,31 @@
+import sys
+import asyncio
+import uuid
+from loguru import logger
+
+# sys.path.insert(0, '/Users/wl/Downloads/Baldwin')
 from gongju import qr_png
 from gongju import LogRegister
 from gongju import WebRequest
-from gongju.pic_download import download_images_from_file
-from gongju import system_monitor
 from gongju.system_monitor import SystemMonitor
-from gongju.trans_geshi import replace_punctuation_in_dir, replace_punctuation_in_file
-import uuid
-from loguru import logger
-import asyncio
+from gongju.dir_file import replace_punctuation_in_dir, replace_punctuation_in_file
 
 only_name = str(uuid.uuid4()).replace('-', '')
 
 
 def mo():
-    asyncio.run(SystemMonitor().monitor())  # 运行监控程序
+    """运行监控程序"""
+    asyncio.run(SystemMonitor().monitor())
 
 
 def tran():
+    """中英文标点转换 文件或者目录维度"""
     replace_punctuation_in_dir("", [])
     replace_punctuation_in_file("/Users/wl/Downloads/Baldwin/xiecheng/async_pro.py")
 
 
 def web():
+    """网络请求封装"""
     url = "https://github.com/jhao104/proxy_pool/blob/master/util/webRequest.py"
     web_req = WebRequest().get(url).text
     print(web_req)
@@ -29,4 +33,5 @@ def web():
 
 if __name__ == '__main__':
     ...
-    tran()
+    # mo()
+    # web()
