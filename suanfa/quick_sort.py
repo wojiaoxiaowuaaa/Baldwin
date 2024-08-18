c1d05f1606a28410911658306fd8bf8629a09662
bugger <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-08-18 15:24:10 +0800

    Update quick_sort.py

--- suanfa/quick_sort.py
+++ suanfa/quick_sort.py
@@ -1,3 +1,6 @@
+import random
+import time
+
 # def quick_sort(arr):
 #     if len(arr) <= 1:
 #         return arr
@@ -38,4 +41,32 @@ def quick_sort(arr):
 
 
 arr = [3, 6, 8, 10, 1, 2, 1, 11, 5, 7, 7, 9, 0, 4, 4, 4, 4]
-print(quick_sort(arr))
+# print(quick_sort(arr))
+
+
+# 冒泡排序 统计耗时
+# def calculate_execution_time(func):
+#     def wrapper_er(*args, **kwargs):
+#         start_time = time.perf_counter()
+#         result = func(*args, **kwargs)
+#         end_time = time.perf_counter()
+#         execution_time = end_time - start_time
+#         print(f"函数 {func.__name__} 的执行时间为: {execution_time} 秒")
+#         return result
+
+#     return wrapper_er
+
+
+# @calculate_execution_time
+def func(l):
+    for i in range(len(l)):
+        for j in range(len(l) - 1 - i):
+            if l[j] > l[j + 1]:
+                l[j], l[j + 1] = l[j + 1], l[j]
+    return l
+
+
+# 生成长度为k的随机列表(从指定序列中随机抽取k个不重复的元素并以列表形式返回这些元素)
+l = random.sample(range(10), 10)
+# print(func(l))
+
