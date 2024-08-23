765754982503db03ec8f8c7a9133d5a9a90f3625
bugger <51432394+wojiaoxiaowuaaa@users.noreply.github.com>
2024-08-23 11:48:13 +0800

    1

--- gongju/api_server.py
+++ gongju/api_server.py
@@ -43,8 +43,6 @@ def upload_file(url, pwd):
 
 # 上传文件时,服务器必须支持文件上传,并正确处理 multipart/form-data 类型的请求.
 # print(upload_file('http://httpbin.org/post', os.path.abspath(__file__)))
-print(upload_file("http://127.0.0.1:5000/upload", os.path.abspath(__file__)))
-
 
 ---------------------------------------------------------------------------------------
 from flask import Flask, request
@@ -57,7 +55,6 @@ def upload():
     file = request.files['file']
     from loguru import logger
     logger.info(file)
-    logger.info(dir(file))
     if file:
         file.save('./uploads/' + file.filename)
         return '文件上传成功'
@@ -73,12 +70,8 @@ import requests
 
 
 def upload_file(url, pwd):
-    with open(
-        pwd, "rb"
-    ) as f:  # 以二进制模式 ('rb') 打开文件,确保文件以字节流的形式读取,这对于上传文件是必要的.f是一个文件对象<_io.BufferedReader name='/Users/wl/Desktop/stork.py'>
-        res = requests.post(
-            url, files={"file": f}
-        )  # requests上传文件.files 参数用于上传文件,{"file": f} 创建了字典,其中键 "file" 是上传文件的字段名,值 f 是文件对象.
+    with open(pwd, "rb") as f:  # 以二进制模式 ('rb') 打开文件,确保文件以字节流的形式读取,这对于上传文件是必要的.f是一个文件对象<_io.BufferedReader name='/Users/wl/Desktop/stork.py'>
+        res = requests.post(url, files={"file": f})  # requests上传文件.files 参数用于上传文件,{"file": f} 创建了字典,其中键 "file" 是上传文件的字段名,值 f 是文件对象.
     return res.text
 
 ---------------------------------------------------------------------------------------
@@ -112,36 +105,70 @@ def login():
 if __name__ == "__main__":
     # 指定端口、host,0.0.0.0代表不管几个网卡,任何ip都可以访问
     app.run(debug=True, port=9999, host="0.0.0.0")
-"""
-import os.path
-from flask import request, Flask
-from loguru import logger
 
-# 上传文件时,服务器必须支持文件上传,并正确处理如 multipart/form-data 等类型的请求.
+---------------------------------------------------------------------------------------
+# 上传文件脚本
+import requests
+from pathlib import Path
+
+files = {"file": open(Path(__file__), 'rb')}  # os.path.abspath(__file__)也能代表当前文件路径
+# print(files)  # {'file': <_io.BufferedReader name='/Users/wl/stork.py'>}
+res = requests.post("http://127.0.0.1:5000/upload", files=files)
+print(res.text)
+
+#  request对象中包含的属性
+['__annotations__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__enter__', '__eq__', '__exit__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_cached_json', '_get_file_stream', '_get_stream_for_parsing', '_load_form_data', '_parse_content_type', '_parsed_content_type', 'accept_charsets', 'accept_encodings', 'accept_languages', 'accept_mimetypes', 'access_control_request_headers', 'access_control_request_method', 'access_route', 'application', 'args', 'authorization', 'base_url', 'blueprint', 'blueprints', 'cache_control', 'close', 'content_encoding', 'content_length', 'content_md5', 'content_type', 'cookies', 'data', 'date', 'dict_storage_class', 'endpoint', 'environ', 'files', 'form', 'form_data_parser_class', 'from_values', 'full_path', 'get_data', 'get_json', 'headers', 'host', 'host_url', 'if_match', 'if_modified_since', 'if_none_match', 'if_range', 'if_unmodified_since', 'input_stream', 'is_json', 'is_multiprocess', 'is_multithread', 'is_run_once', 'is_secure', 'json', 'json_module', 'list_storage_class', 'make_form_data_parser', 'max_content_length', 'max_form_memory_size', 'max_form_parts', 'max_forwards', 'method', 'mimetype', 'mimetype_params', 'on_json_loading_failed', 'origin', 'parameter_storage_class', 'path', 'pragma', 'query_string', 'range', 'referrer', 'remote_addr', 'remote_user', 'root_path', 'root_url', 'routing_exception', 'scheme', 'script_root', 'server', 'shallow', 'stream', 'trusted_hosts', 'url', 'url_root', 'url_rule', 'user_agent', 'user_agent_class', 'values', 'view_args', 'want_form_data_parsed']
+"""
+
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template_string
+from loguru import logger
+import os
+
 app = Flask(__name__)
 
-@app.route('/upload', methods=['POST'])
-def upload():
+UPLOAD_FOLDER = 'uploads'
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER): os.makedirs(UPLOAD_FOLDER)
+
+def allowed_file(filename:str) -> bool:
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template_string('''<!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>''')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    logger.info(request.files)  # ImmutableMultiDict([('file', <FileStorage: '02取换电派单格口.txt' ('text/plain')>)])
+    if 'file' not in request.files:
+        return redirect(request.url)
     file = request.files['file']
-    logger.info(request.files)
-    if file:
-        file.save(os.path.join('./' + file.filename))  # 保存文件到当前路径下
-        return f'文件上传成功 文件名为{file.filename}'  # 返回给请求方的数据
-    else:
-        return '未找到文件'
+    # logger.info(file)  # <FileStorage: '02取换电派单格口.txt' ('text/plain')>
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file', filename=filename))
+    return redirect(request.url)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
 
-# 上传文件脚本
-# import requests
-# from pathlib import Path
-#
-# files = {"file": open(Path(__file__), 'rb')}  # os.path.abspath(__file__)也能代表当前文件路径
-# # print(files)  # {'file': <_io.BufferedReader name='/Users/wl/stork.py'>}
-# res = requests.post("http://127.0.0.1:5000/upload", files=files)
-# print(res.text)
 
-# request对象中包含的属性
-# ['__annotations__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__enter__', '__eq__', '__exit__', '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_cached_json', '_get_file_stream', '_get_stream_for_parsing', '_load_form_data', '_parse_content_type', '_parsed_content_type', 'accept_charsets', 'accept_encodings', 'accept_languages', 'accept_mimetypes', 'access_control_request_headers', 'access_control_request_method', 'access_route', 'application', 'args', 'authorization', 'base_url', 'blueprint', 'blueprints', 'cache_control', 'close', 'content_encoding', 'content_length', 'content_md5', 'content_type', 'cookies', 'data', 'date', 'dict_storage_class', 'endpoint', 'environ', 'files', 'form', 'form_data_parser_class', 'from_values', 'full_path', 'get_data', 'get_json', 'headers', 'host', 'host_url', 'if_match', 'if_modified_since', 'if_none_match', 'if_range', 'if_unmodified_since', 'input_stream', 'is_json', 'is_multiprocess', 'is_multithread', 'is_run_once', 'is_secure', 'json', 'json_module', 'list_storage_class', 'make_form_data_parser', 'max_content_length', 'max_form_memory_size', 'max_form_parts', 'max_forwards', 'method', 'mimetype', 'mimetype_params', 'on_json_loading_failed', 'origin', 'parameter_storage_class', 'path', 'pragma', 'query_string', 'range', 'referrer', 'remote_addr', 'remote_user', 'root_path', 'root_url', 'routing_exception', 'scheme', 'script_root', 'server', 'shallow', 'stream', 'trusted_hosts', 'url', 'url_root', 'url_rule', 'user_agent', 'user_agent_class', 'values', 'view_args', 'want_form_data_parsed']
+
