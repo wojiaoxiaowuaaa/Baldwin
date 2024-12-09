8af9ae77d36bc36085589ff9990ee2027dea4e7e
wb_wanglei41 <wb_wanglei41@meituan.com>
2024-12-09 15:26:49 +0800

    Update xlsx_copy.py
    modify xlsx

--- gongju/xlsx_copy.py
+++ gongju/xlsx_copy.py
@@ -14,12 +14,12 @@ class ExcelModifier:
 
     def duplicate_first_row(self, output_path):
         """
-        将第一行的数据复制到后续行.
+        将第二行的数据复制到后续行.
         :param output_path: 处理后输出文件路径
         """
         first_row_data = [cell.value for cell in self.sheet[2]]
 
-        for row in range(3, 1001):
+        for row in range(3, 10001):
             for col, value in enumerate(first_row_data, start=1):
                 self.sheet.cell(row=row, column=col, value=value)
 
@@ -28,16 +28,23 @@ class ExcelModifier:
 
     def increment_first_column(self, output_path):
         """
-        从第二行开始，每一行第一列的值递增
-        :param output_path: 处理后输的出文件路径
+        从第二行开始，每一行第一列的值递增，其余的列复制第二行的内容
+        :param output_path: 处理后输出文件路径
         """
-        start_value = 8166460001
+        start_value = 8666461001
 
-        # 从第二行开始，每一行第一列的值递增.复制1000遍.
-        for row in range(2, 1001):
-        # for row in range(2, self.sheet.max_row + 1):
+        # 获取第二行的数据（从第二列开始）
+        second_row_data = [cell.value for cell in self.sheet[2]][1:]
+
+        # 从第二行开始，每一行第一列的值递增，并复制第二行的其余列的内容
+        for row in range(2, 10001):
+            # 设置第一列的递增值
             self.sheet.cell(row=row, column=1, value=start_value)
             start_value += 1
+
+            # 复制第二行的其余列内容
+            for col, value in enumerate(second_row_data, start=2):
+                self.sheet.cell(row=row, column=col, value=value)
 
         # 保存修改后的文件
         self.workbook.save(output_path)
@@ -54,5 +61,7 @@ class ExcelModifier:
 
 
 if __name__ == '__main__':
+    ...
     # ExcelModifier('/Users/wl/Downloads/case备份/big_new_file.xlsx').modify_cell(row=2, column=1, value='666')
-    print(ExcelModifier('/Users/wl/Downloads/case备份/big_new_file.xlsx').pd_read_excel())
+    # ExcelModifier('/Users/wl/Downloads/case备份/big_new_file.xlsx').duplicate_first_row(output_path='/Users/wl/Downloads/case备份/big_new_file_1.xlsx')
+    # print(ExcelModifier('/Users/wl/Downloads/case备份/big_new_file.xlsx').pd_read_excel())
