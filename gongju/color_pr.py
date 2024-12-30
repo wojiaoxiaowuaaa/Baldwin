2519f0a39ec8e943afbeea12e423f8ed6487577d
wb_wanglei41 <wb_wanglei41@meituan.com>
2024-12-30 11:39:36 +0800

    color

--- gongju/color_pr.py
+++ gongju/color_pr.py
@@ -30,7 +30,7 @@ def show_memory_info(s="this process"):
     p = psutil.Process(pid)
     info = p.memory_full_info()
     memory = info.uss / 1024.0 / 1024
-    print(" {} pid is {} memory used: {} M".format(s, pid, memory))
+    print("\033[31m {} pid is {} memory used: {} M \033[0m".format(s, pid, memory))
 
 
 def color_print_green(s="*"):
@@ -42,9 +42,29 @@ def color_print_red(s="-"):
     """打印自定义彩色字符"""
     print("\033[1;31m" + s * 30 + "\033[0m")
 
+def show_color_demo():
+    print("\033[31m    \033[0m")
+    print("\033[31mThis is red text\033[0m")
+    print("\033[32mThis is green text\033[0m")
+    print("\033[33mThis is yellow text\033[0m")
+    print("\033[34mThis is blue text\033[0m")
+    print("\033[35mThis is purple text\033[0m")
+    print("\033[36mThis is cyan text\033[0m")
+    print("\033[37mThis is white text\033[0m")
+    print("\033[41m\033[37mThis is white text on red background\033[0m")
+    print("\033[42m\033[37mThis is white text on green background\033[0m")
+    print("\033[43m\033[37mThis is white text on yellow background\033[0m")
+    print("\033[44m\033[37mThis is white text on blue background\033[0m")
+    print("\033[45m\033[37mThis is white text on purple background\033[0m")
+    print("\033[46m\033[37mThis is white text on cyan background\033[0m")
+    print("\033[42m\033[37mThis is white text on green background\033[0m")
+    
+
+
 
 if __name__ == "__main__":
-    color_print_green()
-    color_print_red()
-    print("-------------------分割线-------------------")
+    # color_print_green()
+    # color_print_red()
+    # print("-------------------分割线-------------------")
     show_memory_info()
+    show_color_demo()
