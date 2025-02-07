bfb707a282b9dd0a30ad6381433d0e7ce8497512
wb_wanglei41 <wb_wanglei41@meituan.com>
2025-02-07 14:55:06 +0800

    fix case and update

--- mt_tool.py
+++ mt_tool.py
@@ -1,12 +1,9 @@
 import uuid
-from log_util import logger
+from log_util import logger  # noqa: F401
 from pathlib import Path
-from tool import WebRequest
-from tool import replace_punctuation_in_file
-from tool.secrets_str import main
-from dotenv import load_dotenv
-from config.config import JSON_BIG as js
-import os
+from tool import WebRequest  # noqa: F401
+from tool.secrets_str import main  # noqa: F401
+from config.config import JSON_BIG as js  # noqa: F401
 import toml  # noqa: F401
 
 only_name = str(uuid.uuid4()).replace("-", "")
@@ -15,9 +12,14 @@ wl_dir = Path(__file__).resolve().parents[2]
 
 zsh_file = f"{wl_dir}/.zshrc"
 
-# def get_env():
-#     load_dotenv()
-#     return os.getenv("baldwin")
+def get_env():
+    """获取环境变量"""
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+    return os.getenv("baldwin")
+
+# with open('config/config.toml', 'r') as f: config = toml.load(f)
 
 # def web():
 #     """网络请求封装"""
@@ -27,4 +29,3 @@ zsh_file = f"{wl_dir}/.zshrc"
 
 # replace_punctuation_in_file("")  # 文件标点替换
 
-# with open('config/config.toml', 'r') as f: config = toml.load(f)
