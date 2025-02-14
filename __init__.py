f73b3a8bcb9cef3f2a431d991753293b855cc3b3
wb_wanglei41 <wb_wanglei41@meituan.com>
2025-02-14 16:25:11 +0800

    fix case and update

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-# 通过设置 PYTHONPATH 环境变量,你可以指定 Python 解释器在查找模块和包时应该搜索的目录.这使得你能够在自定义目录中组织和管理你的 Python 模块和包,而不需要将它们复制到标准库路径或当前脚本目录中.  export PYTHONPATH="/Users/wl/Downloads/Baldwin:$PYTHONPATH"
+# 过设置 PYTHONPATH 环境变量,你可以指定 Python 解释器在查找模块和包时应该搜索的目录.这使得你能够在自定义目录中组织和管理你的 Python 模块和包,而不需要将它们复制到标准库路径或当前脚本目录中.  export PYTHONPATH="/Users/wl/Downloads/Baldwin:$PYTHONPATH"
 
 # mac下的brew安装的解释器路径: /usr/local/Cellar/python@3.13/3.13.1/Frameworks/Python.framework/Versions/3.13/bin
 
 # echo $PATH命令在Mac终端(或任何Linux系统)中用于显示当前用户的环境变量PATH的内容.PATH环境变量包含了一系列的目录,这些目录被操作系统用来搜索可执行文件.
+
+# os.system("ls -l") 直接执行命令;home_dir = os.environ.get("HOME")获取主目录;home_dir = os.environ.get("PATH")获取环境变量;
