87bec36113628468c0786fbeb488d371528c394b
wb_wanglei41 <wb_wanglei41@meituan.com>
2025-03-19 11:33:31 +0800

    fix case and update

--- data/pandas_demo.py
+++ data/pandas_demo.py
@@ -3,10 +3,8 @@ import os
 import pandas as pd
 
 # 将根目录加入到系统路径中 否则下一行的导入在终端执行时会报错找不到对应的模块. BASE_PATH : /Users/wl/Downloads/Baldwin
-if (
-    BASE_PATH := os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-) not in sys.path:
-    sys.path.insert(0, BASE_PATH)
+if (BASE_PATH := os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) not in sys.path: sys.path.insert(0, BASE_PATH)
+
 from color_pr import color_print_green
 
 file_path = "/Users/wl/Documents/1675678004817.xlsx"
