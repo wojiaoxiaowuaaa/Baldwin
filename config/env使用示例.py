50668e43a66e8d3c64fbacee8dae06a5b163c353
wb_wanglei41 <wb_wanglei41@meituan.com>
2025-03-20 16:02:03 +0800

    fix case and update

--- config/env使用示例.py
+++ config/env使用示例.py
@@ -1,6 +1,4 @@
 import os
-import json
-from loguru import logger
 from dotenv import load_dotenv
 
 # 加载.env文件
@@ -8,13 +6,14 @@ load_dotenv()
 
 # 访问
 # secret_key = os.environ.get('SECRET_KEY')
-json_demo = os.getenv("JSON_DEMO")
+contract_address = os.getenv("contract_address")
+print(contract_address)
 # 使用这些变量
 # logger.info(f"环境变量: {os.environ}")
 # logger.info(json_demo)
 # logger.info(type(json_demo))  # <class 'str'>
 
-with open(json_demo) as f:
-    data = f.read()
-    # logger.info(type(data))
-    logger.info(data)
+# with open(json_demo) as f:
+#     data = f.read()
+#     # logger.info(type(data))
+#     logger.info(data)
