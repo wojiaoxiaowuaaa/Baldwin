d486cae5697fb7d65159f8c5940cd202a055ad36
wb_wanglei41 <wb_wanglei41@meituan.com>
2025-03-25 10:47:40 +0800

    fix case and update

--- leet_code.py
+++ leet_code.py
@@ -2,7 +2,7 @@
 # from log_util import logger
 
 def move_zero(arr):
-    """双指针.给定一个数组 nums,编写一个函数将所有 0 移动到数组的末尾,同时保持非零元素的相对顺序."""
+    """给定一个数组 nums,编写一个函数将所有 0 移动到数组的末尾,同时保持非零元素的相对顺序.双指针."""
     if not arr: return 0  # noqa: E701
 
     j = 0  # 第一次遍历的时候,j指针记录非0的个数,只要是非0的统统都赋给nums[j]
